5d95eae3af40ecb1e75c8131fad1c87a8854b42a
donhyuck <leedonhyuck123@gmail.com>
2022-04-20 00:05:48 +0900

    테스트 - count변수 다루기

--- src/main/java/com/ldh/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/ldh/exam/demo/controller/UserHomeController.java
@@ -6,22 +6,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserHomeController {
-	@RequestMapping("/user/home/main")
+	private int count;
+
+	@RequestMapping("/user/home/main1")
 	@ResponseBody
-	public String showMain() {
+	public String showMain1() {
 		return "안녕하세요.";
 	}
-	
+
 	@RequestMapping("/user/home/main2")
 	@ResponseBody
 	public String showMain2() {
 		return "반갑습니다.";
 	}
-	
+
 	@RequestMapping("/user/home/main3")
 	@ResponseBody
 	public String showMain3() {
 		return "또 만나요.";
+	}
+
+	@RequestMapping("/user/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		count++;
+		return count;
+	}
+
+	@RequestMapping("/user/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count값을 0으로 초기화";
 	}
 
 }
