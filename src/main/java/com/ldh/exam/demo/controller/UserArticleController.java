291985dccc082d9b15fc8061610ea82750099710
donhyuck <leedonhyuck123@gmail.com>
2022-04-22 16:35:06 +0900

    ArticleService 추가 후 UsrArticleController에 연결

--- src/main/java/com/ldh/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/ldh/exam/demo/controller/UserArticleController.java
@@ -3,18 +3,25 @@ package com.ldh.exam.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ldh.exam.demo.service.ArticleService;
 import com.ldh.exam.demo.vo.Article;
 
 @Controller
 public class UserArticleController {
 
+	// 인스턴스 변수 시작
+	@Autowired
+	private ArticleService articleService;
 	private int articlesLastId;
 	private List<Article> articles;
+	// 인스턴스 변수 끝
 
+	// 생성자
 	public UserArticleController() {
 
 		articlesLastId = 0;
