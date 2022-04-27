d791ab50ab2d31d66f1cfdae4b63fe86f2cbaf57
donhyuck <leedonhyuck123@gmail.com>
2022-04-27 10:16:41 +0900

    회원가입 기능 추가중02, MemberRepository추가

--- src/main/java/com/ldh/exam/demo/controller/UserMemberController.java
+++ src/main/java/com/ldh/exam/demo/controller/UserMemberController.java
@@ -1,5 +1,6 @@
 package com.ldh.exam.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,13 +10,10 @@ import com.ldh.exam.demo.service.MemberService;
 @Controller
 public class UserMemberController {
 
+	@Autowired
 	private MemberService memberService;
 
-	public UserMemberController(MemberService memberService) {
-		this.memberService = memberService;
-	}
-
-	// http://localhost:8011/user/member/doJoin?loginId=a&loginPw=a&name=a&nickname=a&=a&cellphoneNo=11&email1@test.com=a&
+	// http://localhost:8011/user/member/doJoin?loginId=bbb&loginPw=aaa&name=a&nickname=a&=a&cellphoneNo=111&email=a@test.com
 	@RequestMapping("/user/member/doJoin")
 	@ResponseBody
 	public String doJoin(String loginId, String loginPw, String name, String nickname, int cellphoneNo, String email) {
