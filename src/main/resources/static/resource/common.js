4f8f878fcc027a1580b186b6d39b9855de4545f1
donhyuck <leedonhyuck123@gmail.com>
2022-05-04 18:59:49 +0900

    프론트엔드 공통 라이브러리 불러오기/ 제이쿼리, 테일윈드, 폰트어썸 적용

---
+++ src/main/resources/static/resource/common.js
@@ -0,0 +1,30 @@
+/* 지마켓 산스 불러오기 */
+/*
+@font-face {
+    font-family: 'GmarketSansLight';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansLight.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+*/
+
+/* 지마켓 산스 적용하기 */
+/*
+html > body {
+    font-family: "GmarketSansLight";
+    text-underline-position: under;
+}
+*/
+
+/* 에스코어드림Lt */
+@font-face {
+     font-family: 'S-CoreDream-3Light';
+     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
+     font-weight: normal;
+     font-style: normal;
+}
+
+/* 에스코어드림Lt 적용하기 */
+html > body {
+    font-family: "S-CoreDream-3Light";
+}
