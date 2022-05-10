f145d9a987f1de450f264e7fb4c0a35d84482587
donhyuck <leedonhyuck123@gmail.com>
2022-05-10 17:43:18 +0900

    BeforeAction 인터셉터01,  구현

---
+++ src/main/java/com/ldh/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -0,0 +1,17 @@
+package com.ldh.exam.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class BeforeActionInterceptor implements HandlerInterceptor {
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+		System.out.println("============= Interceptor run =============");
+
+		return HandlerInterceptor.super.preHandle(req, resp, handler);
+	}
+}
