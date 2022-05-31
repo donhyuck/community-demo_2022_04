b34adc77db686c16fadfe11d561fb9c2b8ee9152
donhyuck <leedonhyuck123@gmail.com>
2022-05-31 18:32:34 +0900

    로그인 개선 02/ 로그인 후 원래 가려면 페이지로 이동

--- src/main/java/com/ldh/exam/demo/controller/UserMemberController.java
+++ src/main/java/com/ldh/exam/demo/controller/UserMemberController.java
@@ -3,6 +3,7 @@ package com.ldh.exam.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ldh.exam.demo.service.MemberService;
@@ -62,7 +63,7 @@ public class UserMemberController {
 
 	@RequestMapping("/user/member/doLogin")
 	@ResponseBody
-	public String doLogin(String loginId, String loginPw) {
+	public String doLogin(String loginId, String loginPw, @RequestParam(defaultValue = "/") String afterLoginUri) {
 
 		if (Ut.empty(loginId)) {
 			return rq.jsHistoryBack("아이디를 입력해주세요.");
@@ -83,7 +84,7 @@ public class UserMemberController {
 
 		rq.login(member);
 
-		return rq.jsReplace(Ut.format("%s님 환영합니다.", member.getNickname()), "/");
+		return rq.jsReplace(Ut.format("%s님 환영합니다.", member.getNickname()), afterLoginUri);
 	}
 
 	@RequestMapping("/user/member/doLogout")
--- src/main/webapp/WEB-INF/jsp/user/member/login.jsp
+++ src/main/webapp/WEB-INF/jsp/user/member/login.jsp
@@ -7,6 +7,7 @@
 <section>
   <div class="container mx-auto px-3">
     <form class="table-box-type-1" method="post" action="../member/doLogin">
+      <input type="hidden" name="afterLoginUri" value="${ param.afterLoginUri }">
       <table>
         <colgroup>
           <col width="200" />
